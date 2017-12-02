16d317e7dc1fcfdfba1880cc7980e34d0734e07a
Casey Lee <minitopher@gmail.com>
2017-12-02 14:41:15 -0500

    First commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <stdlib.h>
+#include <string>
+#include <cstdlib>
+#include <limits>
+#include <iomanip>
+
+int main(){
+	
+	return 0;
+}
+
