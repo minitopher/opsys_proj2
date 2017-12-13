208359f15b74bb47f050692c04c4f024b72e1c37
Casey Lee <minitopher@gmail.com>
2017-12-13 03:33:44 -0500

    Finished first fit

--- main.cpp
+++ main.cpp
@@ -30,15 +30,15 @@ std::vector<std::string> split(const std::string &s, char delim) {
     return elems;
 }
 
-
+//Prints the memory, make sure you tell it how large the rows are, then how many rows
 void print_mem(std::vector<char> mem, int frames, int lines){
 	std::string memory_block = "";
 	for (int i = 0; i < frames; i++){
 		memory_block += '=';
 	}
 	memory_block += '\n';
-	for (unsigned int i = 0; i < lines; i++){
-		for (unsigned int j = 0; j < frames; j++){
+	for (int i = 0; i < lines; i++){
+		for (int j = 0; j < frames; j++){
 			memory_block += mem[i*frames + j];
 		}
 		memory_block += '\n';
@@ -50,30 +50,53 @@ void print_mem(std::vector<char> mem, int frames, int lines){
 	std::cout << memory_block << std::endl;
 }
 
-void defrag_mem(std::vector<char> &mem){
+//Give it the vector, will defragment it and return how many frames were moved (how much time it takes to move it)
+int defrag_mem(std::vector<char> &mem, int time){
 	
-	//int space_removed = 0;
+	int total = 256;
+	int bytes_moved = 0;
+	std::vector<char> moved;
 
-	int total = 256;	
+	
+	int notmoved = 0;
+	std::cout << notmoved << std::endl;
+	
+	while (mem[notmoved] != '.'){
+		notmoved++;
+	}
 
-	/*for (std::vector<char>::iterator it = mem.begin(); it != mem.end(); it++){
-		if (*it == '.'){
-			mem.erase(it);
-			space_removed++;
+	for (int i = notmoved; i < total; i++){
+		if (mem[i] != '.'){
+			if(std::find(moved.begin(), moved.end(), mem[i]) != moved.end()) {
+				
+			}
+			else{
+				moved.push_back(mem[i]);
+			}
+			bytes_moved++;
 		}
-	}*/
-
+	}
+	std::cout << "time " << time + bytes_moved << "ms: Defragmentation complete (moved " << bytes_moved << " frames: ";
+	for (unsigned int i = 0; i < moved.size() - 1; i ++){
+		std::cout << moved[i] << ", ";
+	}
+	std::cout << moved[moved.size()-1] << ")" << std::endl; 
+	
 	mem.erase(std::remove(mem.begin(), mem.end(), '.'), mem.end());
-
-	for (int i = mem.size(); i < total; i++){
+	
+	for (int i = 0; i < 256 - (notmoved + bytes_moved); i++){
 		mem.push_back('.');
 	}
+
 	
+	//Returns the number of frames it took to do defragmentation, should do a print statement also here.
+	return bytes_moved;
 }
 
+//Returns the total amount of freetime
 int remaining_free(std::vector<char> mem){
 	int count = 0;
-	for (int i = 0; i < mem.size(); i++){
+	for (unsigned int i = 0; i < mem.size(); i++){
 		if (mem[i] == '.'){
 			count++;
 		}
@@ -83,7 +106,7 @@ int remaining_free(std::vector<char> mem){
 }
 
 void remove_proc(std::vector<char> &mem, char proc){
-	for (int i = 0; i < mem.size(); i++){
+	for (unsigned int i = 0; i < mem.size(); i++){
 		if (mem[i] == proc){
 			mem[i] = '.';
 		}
@@ -92,8 +115,9 @@ void remove_proc(std::vector<char> &mem, char proc){
 
 bool check_location(std::vector<char> mem, int start, int len){
 	int space = 0;
-	if (start+len > mem.size()){
-		space = mem.size();
+	//std::cout << start << "	" << len << std::endl;
+	if (start+len > (int)mem.size()){
+		return false;
 		
 	}
 	else{
@@ -124,6 +148,7 @@ void next_fit(std::vector<char> mem, std::vector<Process> processes){
 }
 
 void best_fit(std::vector<char > mem, std::vector<Process> processes){
+	//TODO: Make a function that makes this easy to do, should just make a list of all available spaces
 	
 }
 
@@ -131,44 +156,68 @@ void first_fit(std::vector<char> mem, std::vector<Process> processes){
 	//I'll do this one -Casey
 	
 	int time = 0;
-	int space = mem.size();
+	//int space = mem.size();
 	std::vector<Process> queue;
 
 
 	std::cout << "time " << time << "ms: Simulator started (Contiguous -- Next-Fit)" << std::endl;
 	while (processes.size() != 0){
 		
+		//THIS FUNCTION CHECKS TO SEE IF ANYTHING NEEDS TO BE REMOVED
 		for (unsigned int i = 0; i < processes.size(); i++){
-			std::vector<std::pair<int, int> > proc_hold = processes[i].get_times();
-			if (proc_hold.size() > 0){
-				if (proc_hold[0].second == time - proc_hold[0].first){
-					std::cout << "time " << time << "ms: Process " << processes[i].get_name() << " removed :" << std::endl;
-					print_mem(mem, 32, 8);
-					processes[i].remove_time();
-				}
-			} else{
+			std::pair<int, int> proc_hold = processes[i].get_times();
+			//std::cout << proc_hold.first << "	" << proc_hold.second << std::endl;
+			if (proc_hold.first != -1 && time == proc_hold.first + proc_hold.second){
+				//std::cout << proc_hold.first+proc_hold.second << std::endl;
+				remove_proc(mem, processes[i].get_name());
+				std::cout << "time " << time << "ms: Process " << processes[i].get_name() << " removed:" << std::endl;
+				print_mem(mem, 32, 8);
+				processes[i].remove_time();
+
+			} 
+			else if (proc_hold.first == -1){
 				//no more processes to do, remove this
-				processes.erase(processes.begin() + i-1);
+				processes.erase(processes.begin() + i);
 			}
 		}
 
+		//THIS CHECKS TO SEE IF ANYTHING NEEDS TO BE ADDED / DEFRAGMENTATION
 		for (unsigned int i = 0; i < processes.size(); i++){
-			std::vector<std::pair<int, int> > proc_hold = processes[i].get_times();
-			if (proc_hold[0].first == time){
+			std::pair<int, int> proc_hold = processes[i].get_times();
+			//THIS MEANS THAT SOMETHING IS TO BE ADDED AT THIS TIME
+			if (proc_hold.first == time && proc_hold.first != -1){
+				std::cout << "time " << time << "ms: Process " << processes[i].get_name() << " arrived (requires " << processes[i].get_memframes() << " frames)" << std::endl;
 				//Something is being added	
 				int space_remaining = remaining_free(mem);
+
+				//NOT ENOUGH SPACE, EVEN WITH DEFRAGMENTATION
 				if (processes[i].get_memframes() > space_remaining){
-					//cannot add this process, not enough space
+					//TODO: Skip this process and remove it, its a useless cunt
+					std::cout << "time " << time << "ms: Cannot place process " << processes[i].get_name() << " -- skipped!" << std::endl;
+					print_mem(mem, 32, 8);
+					processes[i].remove_time();
+					//Check if that was the last process to be removed
+					if (processes[i].num_proc() == 0){
+						processes.erase(processes.begin() + i);
+					}
+
+
+				
+					
 				}
 
+				//THERE IS ENOUGH SPACE
 				else{
+					int place_here = 0;
 					bool placed = false;
 					for (unsigned int j = 0; j < mem.size(); j++){
-						if (mem[j] == '.'){								//if we havent started on a 
+						if (mem[j] == '.'){	
+							//FINDS THE FIRST PARTITION THAT IT CAN FIT IN						
 							if (check_location(mem, j, processes[i].get_memframes())){
+								//found a spot where it fits, since this is first fit, use this
+								place_here = (int)j;
 								placed = true;
 								break;
-								//found a spot where it fits
 							}
 							else{
 								while(mem[j] == '.'){
@@ -178,9 +227,45 @@ void first_fit(std::vector<char> mem, std::vector<Process> processes){
 							}
 						}
 					}
+					//IT WAS NOT PLACED, DEFRAG PLS
+					if (!placed){
+						//defragged
+						int offset = 0;
+						offset = defrag_mem(mem, time);
+						time += offset;
+
+						//ADDS ALL THE TIMES FOR THE ARRIVAL TIMES FOR ALL THE PROCESSES
+						for (unsigned int m = 0; m < processes.size(); m++){
+							processes[m].add_arrtime(offset);
+						}
+
+						int defrag_place = 0;
+						while (mem[defrag_place] != '.'){
+							defrag_place++;
+						}
+						for (int n = defrag_place; n < defrag_place + processes[i].get_memframes(); n++){
+							mem[n] = processes[i].get_name();
+						}
+						
+						print_mem(mem, 32, 8);
+
+											
+					}
+					//PLACE THAT SHIT LIKE ITS HOT
+					else{
+						//it placed, move along
+						//TODO: add the actual function you dumb fuck
+						for (int k = place_here; k < place_here+processes[i].get_memframes(); k++){
+							mem[k] = processes[i].get_name();
+						}
+						std::cout << "time " << time << " ms: Placed process " << processes[i].get_name() << ":" <<std::endl;
+						print_mem(mem, 32, 8);
+						
+					}
 				}
 			}
 		}
+		time++;
 	}
 
 	return;
@@ -233,7 +318,7 @@ int main(int argc, char* argv[]){
 			for (unsigned int i = 2; i < temp1.size(); i++){
 				std::vector<std::string> temp2;
 				temp2 = split(temp1[i], '/');
-				std::pair<int,int> temp_pair(std::atoi(temp2[0].c_str()), std::atoi(temp2[0].c_str()));
+				std::pair<int,int> temp_pair(std::atoi(temp2[0].c_str()), std::atoi(temp2[1].c_str()));
 				p->time_pushback(temp_pair);
 			}
 			
@@ -251,8 +336,7 @@ int main(int argc, char* argv[]){
 	//print_mem(testmem,32,8);
 	std::vector<Process> firstfit = processes;
 	
-	
-
+	first_fit(memory, processes);
 	next_fit(memory, processes);
 }
 
--- process.h
+++ process.h
@@ -20,13 +20,29 @@ class Process{
 			return MEM_FRAMES;
 		}
 
-		std::vector<std::pair<int,int> > get_times(){
-			return TIMES;
+		std::pair<int,int> get_times(){
+			
+			std::pair <int, int> fail (-1, -1);
+			if (TIMES.size() == 0){
+				return fail;
+			}
+			return TIMES[0];
 		}
 
 		void remove_time(){
 			TIMES.erase(TIMES.begin());
-		}	
+		}
+
+		//This adds to the time of all the starting times when defragmentation happens and everything needs to get pushed back
+		void add_arrtime(int delay){
+			for (unsigned int i = 0; i < TIMES.size(); i++){
+				TIMES[i].first += delay;
+			}
+		}
+
+		int num_proc(){
+			return (int)TIMES.size();
+		}
 		
 	private:
 		char PROC_ID;
